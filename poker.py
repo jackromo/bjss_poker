2af17584ca54999b5bfb339d6410a873b57402bf
Jack Romo <sharrackor@gmail.com>
2016-02-25 00:42:36 +0000

    Enforce provision of number and suit in PokerCard instantiation

--- poker.py
+++ poker.py
@@ -6,11 +6,12 @@ class PokerCard(object):
     suits = ["C", "D", "H", "S"]
 
     def __init__(self, val):
+        if not isinstance(val, str):
+            raise TypeError("input must be a string")
+        elif len(val) != 2:
+            raise ValueError("input must be 2 chars long, of number and suit")
         self.val = val[0]
-        if len(val) == 1:
-            self.suit = "H"
-        else:
-            self.suit = val[1]
+        self.suit = val[1]
 
     def __lt__(self, other):
         if other == None:   # None is default lowest possible card.
@@ -37,7 +38,7 @@ class PokerHand(object):
         pairs = []
         for c_val, count in zip(PokerCard.cards, vals_count):
             if count == 2:
-                pairs.append(PokerCard(c_val))
+                pairs.append(PokerCard(c_val+"H"))  # Suit is irrelevant to result, only care about number
         return pairs
 
     def has_1pair(self):
@@ -53,7 +54,7 @@ class PokerHand(object):
         threes_val = None
         for c_val, count in zip(PokerCard.cards, vals_count):
             if count == 3:
-                threes_val = PokerCard(c_val)
+                threes_val = PokerCard(c_val+"H")
         return threes_val
 
     def has_3kind(self):
@@ -66,7 +67,7 @@ class PokerHand(object):
         fours_val = None
         for c_val, count in zip(PokerCard.cards, vals_count):
             if count == 4:
-                fours_val = PokerCard(c_val)
+                fours_val = PokerCard(c_val+"H")
         return fours_val
 
     def has_4kind(self):
@@ -150,84 +151,84 @@ class PokerTester(unittest.TestCase):
     def test_card_compare(self):
         for i, c1_val in enumerate(PokerCard.cards):
             for j, c2_val in enumerate(PokerCard.cards[i+1:]):
-                card1 = PokerCard(c1_val)
-                card2 = PokerCard(c2_val)
+                card1 = PokerCard(c1_val+"H")
+                card2 = PokerCard(c2_val+"H")
                 assert(card1 < card2)
                 assert(max(card1, card2).val == c2_val)
-            assert(PokerCard(c1_val) == PokerCard(c1_val))
+            assert(PokerCard(c1_val+"H") == PokerCard(c1_val+"H"))
 
     def test_card_sorted(self):
         card_vals = PokerCard.cards
-        cards = [PokerCard("2")] + [PokerCard(val) for val in list(reversed(card_vals))]
+        cards = [PokerCard("2C")] + [PokerCard(val + "C") for val in list(reversed(card_vals))]
         sorted_vals = sorted(cards)
-        assert([c.val for c in sorted_vals] == ["2"] + card_vals)
+        assert([c.val + c.suit for c in sorted_vals] == ["2C"] + [c+"C" for c in card_vals])
 
     def test_hand_1pair(self):
-        hand = PokerHand(["2", "3", "4", "4", "5"])
+        hand = PokerHand(["2H", "3H", "4H", "4H", "5H"])
         assert(hand.has_1pair())
-        hand2 = PokerHand(["2", "3", "4", "5", "6"])
+        hand2 = PokerHand(["2H", "3H", "4H", "5H", "6H"])
         assert(not hand2.has_1pair())
-        hand3 = PokerHand(["2", "3", "3", "3", "6"])
+        hand3 = PokerHand(["2H", "3H", "3H", "3H", "6H"])
         assert(not hand3.has_1pair())
 
     def test_hand_get_pair_vals(self):
-        hand = PokerHand(["4", "2", "2", "3", "3"])
-        assert(hand.get_pairs() == [PokerCard("2"), PokerCard("3")])
+        hand = PokerHand(["4H", "2H", "2H", "3H", "3H"])
+        assert(hand.get_pairs() == [PokerCard("2H"), PokerCard("3H")])
 
     def test_hand_2pairs(self):
-        hand = PokerHand(["5", "3", "3", "4", "4"])
+        hand = PokerHand(["5H", "3H", "3H", "4H", "4H"])
         assert(hand.has_2pairs())
-        hand2 = PokerHand(["2", "3", "4", "4", "5"])
+        hand2 = PokerHand(["2H", "3H", "4H", "4H", "5H"])
         assert(not hand2.has_2pairs())
-        hand3 = PokerHand(["2", "3", "4", "5", "6"])
+        hand3 = PokerHand(["2H", "3H", "4H", "5H", "6H"])
         assert(not hand3.has_2pairs())
 
     def test_hand_3kind(self):
-        hand = PokerHand(["5", "3", "3", "3", "4"])
+        hand = PokerHand(["5H", "3H", "3H", "3H", "4H"])
         assert(hand.has_3kind())
-        hand2 = PokerHand(["2", "3", "4", "4", "5"])
+        hand2 = PokerHand(["2H", "3H", "4H", "4H", "5H"])
         assert(not hand2.has_3kind())
 
     def test_hand_get_3kind(self):
-        hand = PokerHand(["5", "3", "3", "3", "4"])
-        assert(hand.get_3kind() == PokerCard("3"))
-        hand2 = PokerHand(["2", "3", "4", "4", "5"])
+        hand = PokerHand(["5H", "3H", "3H", "3H", "4H"])
+        assert(hand.get_3kind() == PokerCard("3H"))
+        hand2 = PokerHand(["2H", "3H", "4H", "4H", "5H"])
         assert(hand2.get_3kind() == None)
 
     def test_hand_4kind(self):
-        hand = PokerHand(["2", "4", "4", "4", "4"])
+        hand = PokerHand(["2H", "4H", "4H", "4H", "4H"])
         assert(hand.has_4kind())
-        hand2 = PokerHand(["2", "3", "4", "5", "6"])
+        hand2 = PokerHand(["2H", "3H", "4H", "5H", "6H"])
         assert(not hand2.has_4kind())
 
     def test_hand_get_4kind(self):
-        hand = PokerHand(["5", "3", "3", "3", "3"])
-        assert(hand.get_4kind() == PokerCard("3"))
-        hand2 = PokerHand(["2", "3", "4", "4", "4"])
+        hand = PokerHand(["5H", "3H", "3H", "3H", "3H"])
+        assert(hand.get_4kind() == PokerCard("3H"))
+        hand2 = PokerHand(["2H", "3H", "4H", "4H", "4H"])
         assert(hand2.get_4kind() == None)
 
     def test_hand_full_house(self):
-        hand = PokerHand(["3", "3", "3", "2", "2"])
+        hand = PokerHand(["3H", "3H", "3H", "2H", "2H"])
         assert(hand.has_full_house())
-        hand2 = PokerHand(["2", "3", "4", "5", "6"])
+        hand2 = PokerHand(["2H", "3H", "4H", "5H", "6H"])
         assert(not hand2.has_full_house())
 
     def test_hand_get_full_house(self):
-        hand = PokerHand(["5", "5", "3", "3", "3"])
-        assert(hand.get_full_house() == PokerCard("3"))
-        hand2 = PokerHand(["2", "3", "4", "4", "4"])
+        hand = PokerHand(["5H", "5H", "3H", "3H", "3H"])
+        assert(hand.get_full_house() == PokerCard("3H"))
+        hand2 = PokerHand(["2H", "3H", "4H", "4H", "4H"])
         assert(hand2.get_full_house() == None)
 
     def test_hand_straight(self):
-        hand = PokerHand(["2", "3", "4", "5", "6"])
+        hand = PokerHand(["2H", "3H", "4H", "5H", "6H"])
         assert(hand.has_straight())
-        hand2 = PokerHand(["2", "3", "5", "6", "7"])
+        hand2 = PokerHand(["2H", "3H", "5H", "6H", "7H"])
         assert(not hand2.has_straight())
 
     def test_hand_get_straight(self):
-        hand = PokerHand(["2", "3", "4", "5", "6"])
-        assert(hand.get_straight() == PokerCard("6"))
-        hand2 = PokerHand(["2", "3", "5", "6", "7"])
+        hand = PokerHand(["2H", "3H", "4H", "5H", "6H"])
+        assert(hand.get_straight() == PokerCard("6H"))
+        hand2 = PokerHand(["2H", "3H", "5H", "6H", "7H"])
         assert(hand2.get_straight() == None)
 
     def test_hand_flush(self):
